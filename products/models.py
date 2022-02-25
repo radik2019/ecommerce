b620adf675828fdb29674f556798d71783ae5dd6
Radu Cebotari <r.cebotari@apptoyou.it>
2022-02-25 14:47:49 +0100

    [ADD] Post

--- products/models.py
+++ products/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 
+
 class Product(models.Model):
+    hash_summ = models.CharField(max_length=255, unique=True, default='')
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.01)
     availability = models.PositiveIntegerField(default=0)
--- products/views.py
+++ products/views.py
@@ -3,10 +3,15 @@ from django.forms.models import model_to_dict
 from django.http.response import JsonResponse
 from .models import Product, Brand, Category
 import json
+from hashlib import sha256
 from django.views.decorators.csrf import csrf_exempt
 
+
 def debug_(s):
-    print("*" * 50, "\n", s, '\n', "*" * 50)
+    import os
+    width_wind = os.get_terminal_size().columns -5
+    print("*" * width_wind, "\n\n", s, '\n\n' + "*" * width_wind)
+
 
 class RetrieveRESTViewMixin:
     def get_detail(self, request, pk=None, *args, **kwargs):
@@ -14,6 +19,7 @@ class RetrieveRESTViewMixin:
         if not resource.exists():
             return JsonResponse({"detail": "Resource not found"}, status=404)
         return JsonResponse(model_to_dict(resource.first()), status=200, safe=False)
+
 
 class DeleteRESTViewMixin:
     def del_detail(self, request, pk=None, *args, **kwargs):
@@ -23,6 +29,7 @@ class DeleteRESTViewMixin:
         resource_name = resource.first()
         resource_name.first().delete()
         return JsonResponse({"detail": f"[{resource_name}] has been deleted"}, status=200, safe=False)
+
 
 class ListRESTViewMixin:
     def get_list(self, request, *args, **kwargs):
@@ -53,11 +60,16 @@ class RESTView(View):
     def http_method_not_allowed(self, request, *args, **kwargs):
         return JsonResponse({"detail": "Method not allowed!"}, status=405)
 
-
-
-
 class ProductView(RESTView, ListRESTViewMixin, RetrieveRESTViewMixin, DeleteRESTViewMixin):
     model = Product 
+    
+    def hash_product(self, req_payload):
+        name: str = req_payload.get("name")
+        category = Category.objects.get(cat_name=req_payload.get("category")).cat_name
+        brand = Brand.objects.get(model_name=req_payload.get("brand")).model_name
+        s = (name + category + brand).encode(encoding='utf-8')
+        hashed = sha256(s).hexdigest()
+        return hashed
     
     def post(self, request):
         """
@@ -72,9 +84,11 @@ class ProductView(RESTView, ListRESTViewMixin, RetrieveRESTViewMixin, DeleteREST
         task = None
         try:
             req_payload = json.loads(request.body)
+            debug_(req_payload)
             task = Product.objects.create(
+                hash_summ = self.hash_product(req_payload),
                 name=req_payload.get("name"),
-                price=req_payload.get("price", ""),
+                price=req_payload.get("price"),
                 availability=int(req_payload.get("availability")),
                 category=Category.objects.get(cat_name=req_payload.get("category")),
                 brand=Brand.objects.get(model_name=req_payload.get("brand"))
@@ -87,6 +101,24 @@ class ProductView(RESTView, ListRESTViewMixin, RetrieveRESTViewMixin, DeleteREST
             )
         return JsonResponse(model_to_dict(task), status=201)
 
+    def put(self, request, pk, *args):
+        """_summary_
+        {
+            "id": 8,
+            "name": "Qualita' Cremagusto",
+            "price": 4.01,
+            "availability": 10,
+            "category": 2,
+            "brand": 5
+        }
+        """
+        debug_(json.loads(request.body))
+        task = request.body
+        debug_(self.get_product_dict(pk))
+        
+        return JsonResponse(
+                {"error": "Errore nella modifica del modello"},
+                status=500)
 
 
 class BrandView(RESTView, ListRESTViewMixin, RetrieveRESTViewMixin, DeleteRESTViewMixin):
